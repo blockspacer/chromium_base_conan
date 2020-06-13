82c2980fc0ec2fbc66fd6baf699424330d3e0510
CI_Build_Bot <cibuildbot@cibuildbot.com>
2020-06-13 15:11:31 +0300

    REFACTOR: cmake

--- cmake/setup_sanitizers.cmake
+++ cmake/setup_sanitizers.cmake
@@ -1,33 +1,19 @@
 ﻿include_guard( DIRECTORY )
 
-# Helper module for selecting option based on multiple values. Module help:
-# https://cmake.org/cmake/help/latest/module/CMakeDependentOption.html
-include(CMakeDependentOption)
-
 # Hack to depend NOT on CONAN_PKG::libname, but on libname directly
 # See for details
 # https://docs.conan.io/en/latest/developing_packages/workspaces.html
-set(LOCAL_BUILD FALSE CACHE BOOL "LOCAL_BUILD")
-message(STATUS "LOCAL_BUILD=${LOCAL_BUILD}")
+set(${PROJECT_NAME}_LOCAL_BUILD FALSE CACHE BOOL "${PROJECT_NAME}_LOCAL_BUILD")
+message(STATUS "${PROJECT_NAME}_LOCAL_BUILD=${${PROJECT_NAME}_LOCAL_BUILD}")
 
-set(BASE_USE_PARTITION_ALLOCATOR TRUE CACHE BOOL "BASE_USE_PARTITION_ALLOCATOR")
-
-set(ENABLE_UKM FALSE CACHE BOOL "ENABLE_UKM")
-
-set(USE_ALLOC_SHIM FALSE CACHE BOOL "USE_ALLOC_SHIM")
-
-set(USE_DEB_ALLOC FALSE CACHE BOOL "USE_DEB_ALLOC")
-
-set(BASE_USE_JSON TRUE CACHE BOOL "BASE_USE_JSON")
-
-set(BASE_SOURCES_PATH ${CMAKE_CURRENT_SOURCE_DIR}/base/)
-
-set(BUILD_CONFIG_PARENT_PATH ${CMAKE_CURRENT_SOURCE_DIR}/codegen/build/) # TODO: use shared BUILD_CONFIG
-
-set(BUILDFLAGS_GENERATORS_PATH ${CMAKE_CURRENT_SOURCE_DIR}/codegen/)
+option(ENABLE_TESTS "Enable tests" OFF)
 
 option(ENABLE_VALGRIND
   "Enable valgrind" OFF)
+
+# TODO: __do_global_dtors_aux, base::debug::CollectStackTrace
+option(ENABLE_VALGRIND_TESTS
+  "Enable valgrind for unit tests" OFF)
 
 # see https://github.com/Ericsson/codechecker/blob/master/tools/report-converter/README.md#undefined-behaviour-sanitizer
 # NOTE: Compile with -g and -fno-omit-frame-pointer
@@ -61,29 +47,3 @@ option(ENABLE_MSAN
 # to get proper debug information in your binary.
 option(ENABLE_TSAN
   "Enable Thread Sanitizer" OFF)
-
-cmake_dependent_option(ALLOCATOR_TCMALLOC
-  "Disable custom allocators (use glibc on linux)"
-  ON
-  "NOT ENABLE_ASAN \
-  AND NOT ENABLE_TSAN \
-  AND NOT ENABLE_MSAN \
-  AND NOT ENABLE_UBSAN \
-  AND NOT ENABLE_VALGRIND"
-  OFF)
-
-message(STATUS
-  "ALLOCATOR_TCMALLOC=${ALLOCATOR_TCMALLOC}")
-
-cmake_dependent_option(ALLOCATOR_NONE
-  "Disable custom allocators (use glibc on linux)"
-  ON
-  "ENABLE_ASAN \
-  OR ENABLE_TSAN \
-  OR ENABLE_MSAN \
-  OR ENABLE_UBSAN \
-  OR ENABLE_VALGRIND"
-  OFF)
-
-message(STATUS
-  "ALLOCATOR_NONE=${ALLOCATOR_NONE}")
